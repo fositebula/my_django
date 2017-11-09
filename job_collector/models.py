94db800de640e281509290e65796037d7b1b7d6d
dongpl <dongpl@spreadst.com>
2017-11-09 19:48:08 +0800

    增加了submitter守护进程, 可以从数据库中读取数据

--- job_collector/models.py
+++ job_collector/models.py
@@ -5,6 +5,28 @@ from django.db import models
 
 # Create your models here.
 class CollectInfos(models.Model):
+
+    NO_SUBMIT = 0
+    SUBMITTED = 1
+
+    STATUS_CHOICE = (
+        (NO_SUBMIT, "No submit"),
+        (SUBMITTED, "Submitted"),
+    )
+
+    STATUS_MAP={
+        "No submit": NO_SUBMIT,
+        "Submitted": SUBMITTED
+    }
+
+    SUBMITTED_SUCCESSFULLY = 0
+    SUBMITTED_FAILED = 1
+
+    SUBMITTED_CHOICE = (
+        (SUBMITTED_SUCCESSFULLY, "Submitted successfully"),
+        (SUBMITTED_FAILED, "Submitted failed"),
+    )
+
     branch = models.CharField('branch', max_length=50)
     project = models.CharField('project', max_length=50)
     buildid = models.CharField('buildid', max_length=50)
@@ -20,6 +42,61 @@ class CollectInfos(models.Model):
     test_description = models.CharField('test_description', max_length=50)
     project_num = models.CharField('project_num', max_length=50)
     test_task_type = models.CharField('test_task_type', max_length=50)
+    bugid = models.CharField('bugid to gerrit', max_length=200)
+    repository = models.CharField('code repository', max_length=100)
+
+    from_verify_server_time =models.DateTimeField(auto_now=True)
+    submit_time = models.DateTimeField(
+        verbose_name=u"submit time",
+        auto_now=False,
+        auto_now_add=False,
+        null=True,
+        blank=True,
+        editable=False
+    )
+
+    status = models.IntegerField(
+        verbose_name=u'submit status',
+        default=NO_SUBMIT, choices=STATUS_CHOICE
+    )
+
+    filted = models.BooleanField(default=False)
+
+    submitted_result = models.IntegerField(
+        verbose_name=u'submitted result',
+        help_text=u'submitted task can be successfully or fail',
+        default=SUBMITTED_SUCCESSFULLY, choices=SUBMITTED_CHOICE,
+    )
+    submitted_fail_reason = models.CharField(
+        verbose_name='submitted fail reason',
+        help_text=u'submitted fail reason, if fail you can input some words mark it',
+        max_length=100,
+    )
 
     def __str__(self):
         return "%s---%s---%s---%s"%(self.buildid, self.branch, self.project, self.gerrit_id)
+
+class TestJob(models.Model):
+
+    COMPLETE = 0
+    INCOMPLETE = 1
+
+    JOB_STATUS_CHOICE = (
+        (COMPLETE, "complete"),
+        (INCOMPLETE, "incomplete"),
+    )
+
+    jobid = models.CharField(verbose_name='jobid', max_length=10, default=None)
+    job_status = models.IntegerField(
+        verbose_name='job status',
+        default=COMPLETE,
+        choices=JOB_STATUS_CHOICE
+    )
+    testcase = models.CharField(
+        help_text=u"formate ${casename}:${result}",
+        verbose_name='buildid', max_length=200, default=None
+    )
+    collect_infos = models.ForeignKey('CollectInfos')
+
+    def __str__(self):
+        return "TestJob: %s"%jobid
