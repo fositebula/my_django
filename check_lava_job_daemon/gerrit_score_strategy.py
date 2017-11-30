e476402eb344e9ebb3d7c1de6b168cc47ae89354
fositebula <838177065@qq.com>
2017-11-30 15:47:37 +0800

    删除lava_submission_bak

--- check_lava_job_daemon/gerrit_score_strategy.py
+++
@@ -1,3 +0,0 @@
-
-def gerrt_score():
-    pass
--- job_collector/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.contrib import admin
-from job_collector.models import CollectInfos, TestJob
-# Register your models here.
-admin.site.register(CollectInfos)
-admin.site.register(TestJob)
